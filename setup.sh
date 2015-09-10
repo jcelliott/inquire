d3c113ff6c316a961691fc17257c77e48aa032e6
Joshua Elliott <joshuacelliott@gmail.com>
2015-09-09 21:14:04 -0600

    Use Stanford NER in docker container

--- setup.sh
+++ setup.sh
@@ -2,8 +2,4 @@
 # setup for question answering app
 
 # start Stanford NER server
-_ner_dir='resources/stanford-ner'
-java -mx700m -cp "$_ner_dir/stanford-ner.jar" edu.stanford.nlp.ie.NERServer \
-  -loadClassifier "$_ner_dir/classifiers/english.muc.7class.distsim.crf.ser.gz" \
-  -port 9090 \
-  -outputFormat inlineXML
+docker run -d -p 9090:8080 jcelliott/ner
